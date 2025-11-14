808be0cebba62072e746c3b9999b6ce7692dfccf
Guillermo Gomez <guillermo.gomez@franka.de>
2025-11-14 14:06:18 +0100

    feat: reduced kinematic version for tmr v02 added

--- .docker/create_urdf.entrypoint.sh
+++ .docker/create_urdf.entrypoint.sh
@@ -6,7 +6,7 @@ shift $#
 source /ros_entrypoint.sh
 
 cd /workspaces
-colcon build --packages-select franka_description olv_module_descriptions > /dev/null
+colcon build --packages-select franka_description > /dev/null
 source install/setup.bash
 
 cd src/franka_description
