987775e8587394bfc9c97bb1bd304b987f0f70eb
Guillermo Gomez <guillermo.gomez@franka.de>
2025-11-24 21:40:34 +0100

    fix: joint names modified for ros2 control

--- .docker/visualize_franka_duo.entrypoint.sh
+++ .docker/visualize_franka_duo.entrypoint.sh
@@ -4,7 +4,7 @@ args=$*
 shift $#
 
 cd /workspaces
-colcon build --packages-select franka_description > /dev/null
+colcon build --packages-select franka_description olv_module_descriptions > /dev/null
 source install/setup.bash
 
 ros2 launch franka_description visualize_franka_duo.launch.py ${args}
